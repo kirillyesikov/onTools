f50424b9a44232080da27b5b2b071211053f2412
kirillyesikov <kyesikov@gmail.com>
2024-12-12 23:05:58 -0500

    Trying React here

---
+++ img/index.jsx
@@ -0,0 +1,266 @@
+import React from "react";
+import { Arr } from "../Arr/Arr";
+import { HstNumberWillNotBeRequiredAtThisMomentButWillBeNeededToStartWorkingThroughOurAgency } from "../HstNumberWillNotBeRequiredAtThisMomentButWillBeNeededToStartWorkingThroughOurAgency/HstNumberWillNotBeRequiredAtThisMomentButWillBeNeededToStartWorkingThroughOurAgency";
+import { IconComponentNode } from "../IconComponentNode/IconComponentNode";
+import { Logo } from "../Logo/Logo";
+import { Vector } from "../Vector/Vector";
+import "./style.css";
+
+export const WorkerTeamAccount = () => {
+  return (
+    <div className="WORKER-TEAM-ACCOUNT">
+      <div className="div">
+        <HstNumberWillNotBeRequiredAtThisMomentButWillBeNeededToStartWorkingThroughOurAgency className="welcome-to-ontools" />
+        <div className="button-search">
+          <div className="text-wrapper">GO BACK</div>
+
+          <div className="ellipse" />
+        </div>
+
+        <div className="overlap">
+          <div className="button-search-2">
+            <div className="text-wrapper">ALREADY A MEMBER</div>
+
+            <div className="ellipse" />
+          </div>
+
+          <div className="button-search-2">
+            <div className="text-wrapper">ALREADY A MEMBER</div>
+
+            <div className="ellipse" />
+          </div>
+
+          <div className="button-search-2">
+            <div className="text-wrapper">ALREADY A MEMBER</div>
+
+            <div className="ellipse" />
+          </div>
+
+          <div className="button-search-2">
+            <div className="text-wrapper">ALREADY A MEMBER</div>
+
+            <div className="ellipse" />
+          </div>
+
+          <button className="button">
+            <div className="text-wrapper">CONTINUE</div>
+
+            <div className="ellipse" />
+          </button>
+        </div>
+
+        <div className="overlap-group">
+          <div className="input">
+            <div className="input-components">
+              <div className="input-2">
+                <p className="first-name">
+                  543 Lanky Court, yellownife, NWT, x6r 5rd
+                </p>
+              </div>
+
+              <p className="first-name-2">
+                Personal full address including postal code
+              </p>
+            </div>
+          </div>
+
+          <div className="text-wrapper-2">*</div>
+        </div>
+
+        <div className="overlap-2">
+          <div className="input-components-wrapper">
+            <div className="input-components-2">
+              <div className="input-3">
+                <div className="first-name-3">Sunflowers paint</div>
+              </div>
+
+              <p className="first-name-2">
+                Choose a Username (company name works)
+              </p>
+            </div>
+          </div>
+
+          <div className="text-wrapper-3">*</div>
+        </div>
+
+        <div className="overlap-3">
+          <div className="input">
+            <div className="input-components">
+              <div className="input-2">
+                <div className="first-name">wrtgwrtg</div>
+              </div>
+
+              <div className="first-name-2">Enter a password</div>
+            </div>
+          </div>
+
+          <div className="text-wrapper-4">*</div>
+        </div>
+
+        <div className="overlap-4">
+          <div className="div-wrapper">
+            <div className="input-components-3">
+              <div className="input-4">
+                <div className="first-name-4">wrtgwrt</div>
+              </div>
+
+              <div className="first-name-2">Confirm your password</div>
+            </div>
+          </div>
+
+          <div className="text-wrapper-5">*</div>
+        </div>
+
+        <div className="overlap-5">
+          <div className="input">
+            <div className="input-components">
+              <div className="input-2">
+                <div className="first-name">Ginette Reno</div>
+              </div>
+
+              <input
+                className="first-name-5"
+                placeholder="Your full name"
+                type="text"
+              />
+            </div>
+          </div>
+
+          <div className="text-wrapper-6">*</div>
+        </div>
+
+        <div className="overlap-6">
+          <div className="input-5">
+            <div className="input-components-2">
+              <div className="input-3">
+                <div className="first-name-3">Sunflowers paint</div>
+              </div>
+
+              <p className="first-name-2">Company full name (If any)</p>
+            </div>
+          </div>
+
+          <div className="input-5">
+            <div className="input-components-2">
+              <div className="input-3">
+                <div className="first-name-3">Sunflowers paint inc.-</div>
+              </div>
+
+              <p className="first-name-2">Company full name (If any)</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="input-6">
+          <div className="input-components-4">
+            <div className="input-7">
+              <div className="first-name-6">4565 4564 4564 rt0001</div>
+            </div>
+
+            <div className="first-name-2">GST/HST #</div>
+          </div>
+        </div>
+
+        <div className="overlap-7">
+          <div className="input">
+            <div className="input-components">
+              <div className="input-2">
+                <input
+                  className="first-name-7"
+                  placeholder="ginette@sunflowers.com"
+                  type="email"
+                />
+              </div>
+
+              <input
+                className="first-name-5"
+                placeholder="Email address"
+                type="email"
+              />
+            </div>
+          </div>
+
+          <div className="text-wrapper-7">*</div>
+        </div>
+
+        <div className="overlap-8">
+          <div className="input-8">
+            <div className="input-components-3">
+              <div className="input-4">
+                <div className="first-name-4">438-882-2537</div>
+              </div>
+
+              <input
+                className="first-name-5"
+                placeholder="Phone number"
+                type="tel"
+              />
+            </div>
+          </div>
+
+          <div className="text-wrapper-8">*</div>
+        </div>
+
+        <p className="p">Let’s get started with you!</p>
+
+        <div className="overlap-9">
+          <div className="text-wrapper-9">*</div>
+
+          <div className="input-9">
+            <div className="input-components-5">
+              <div className="overlap-group-wrapper">
+                <div className="overlap-group-2">
+                  <div className="first-name-8">female</div>
+
+                  <IconComponentNode className="arr-instance" />
+                </div>
+              </div>
+
+              <div className="first-name-9">Gender</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="overlap-10">
+          <div className="text-wrapper-10">*</div>
+
+          <p className="text-wrapper-11">
+            I read and accept Ontools Agency terms and conditions
+          </p>
+
+          <div className="text-wrapper-12">TERMS AND CONDITIONS</div>
+        </div>
+
+        <Vector className="vector-instance" />
+        <div className="input-10">
+          <div className="input-components-5">
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <div className="first-name-8">Corporation</div>
+
+                <Arr className="arr-instance" />
+              </div>
+            </div>
+
+            <div className="first-name-9">Company type</div>
+          </div>
+        </div>
+
+        <div className="menu">
+          <div className="menu-2">
+            <Logo className="logo-instance" />
+            <div className="navbar">
+              <div className="text-wrapper-13">Home</div>
+
+              <div className="text-wrapper-13">Work with us</div>
+
+              <div className="text-wrapper-13">About us</div>
+
+              <div className="text-wrapper-13">Contacts</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ my-react-app/src/Arr.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const Arr = ({ className }) => {
+  return (
+    <svg
+      className={`arr ${className}`}
+      fill="none"
+      height="20"
+      viewBox="0 0 20 20"
+      width="20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path"
+        clipRule="evenodd"
+        d="M11.3215 10L7.74408 6.42259C7.41864 6.09715 7.41864 5.56952 7.74408 5.24408C8.06951 4.91864 8.59715 4.91864 8.92259 5.24408L13.0893 9.41074C13.4147 9.73618 13.4147 10.2638 13.0893 10.5893L8.92259 14.7559C8.59715 15.0814 8.06951 15.0814 7.74408 14.7559C7.41864 14.4305 7.41864 13.9028 7.74408 13.5774L11.3215 10Z"
+        fill="#9EAFB9"
+        fillRule="evenodd"
+      />
+
+      <mask
+        className="mask"
+        height="10"
+        id="mask0_0_26"
+        maskUnits="userSpaceOnUse"
+        width="7"
+        x="7"
+        y="5"
+      >
+        <path
+          className="path"
+          clipRule="evenodd"
+          d="M11.3215 10L7.74408 6.42259C7.41864 6.09715 7.41864 5.56952 7.74408 5.24408C8.06951 4.91864 8.59715 4.91864 8.92259 5.24408L13.0893 9.41074C13.4147 9.73618 13.4147 10.2638 13.0893 10.5893L8.92259 14.7559C8.59715 15.0814 8.06951 15.0814 7.74408 14.7559C7.41864 14.4305 7.41864 13.9028 7.74408 13.5774L11.3215 10Z"
+          fill="white"
+          fillRule="evenodd"
+        />
+      </mask>
+
+      <g className="g" mask="url(#mask0_0_26)" />
+    </svg>
+  );
+};
